3651dadab28aef23279039a3d8d531d528b636c3
GabrielInacio <gabrielinacio@id.uff.br>
2020-08-24 19:21:37 -0300

    Cross Origin tests

--- src/main/java/br/com/GabrielIDSM/EncryptorAPI/Controller/MessageController.java
+++ src/main/java/br/com/GabrielIDSM/EncryptorAPI/Controller/MessageController.java
@@ -27,11 +27,13 @@ import br.com.GabrielIDSM.EncryptorAPI.Model.MessageRequestModelToEnigmaTirpitz;
 import br.com.GabrielIDSM.EncryptorAPI.Model.MessageRequestModelToEnigmaZahlwerk;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("message")
 public class MessageController {
